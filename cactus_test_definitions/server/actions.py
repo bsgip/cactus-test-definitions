8bdf92b95ad11de4dedb4227400b86427b871d39
Josh Vote <jjvote@gmail.com>
2026-02-18 13:35:46 +1100

    Simplified

--- cactus_test_definitions/server/actions.py
+++ cactus_test_definitions/server/actions.py
@@ -52,9 +52,7 @@ ACTION_PARAMETER_SCHEMA: dict[str, dict[str, ParameterSchema]] = {
     },  # Waits (doing nothing - blocking other step actions) until the specified time period has passed
     "refresh-resource": {
         "resource": ParameterSchema(True, ParameterType.CSIPAusResource),
-        "expect_rejection": ParameterSchema(
-            False, ParameterType.Boolean
-        ),  # True - expect 4XX and ErrorPayload. False - allow errors to retry. None - must succeed and no retries
+        "expect_rejection": ParameterSchema(False, ParameterType.Boolean),  # True - expect 4XX and ErrorPayload.
         "expect_rejection_or_empty": ParameterSchema(
             False, ParameterType.Boolean
         ),  # Similar to expect_rejection but also allow en empty list (if it's a list resource)
