5481794b4b33b63423d226700fb392de2501cf15
LachlanJW <watsonjlachlan@gmail.com>
2026-08-11 11:36:52 +1000

    Tagging 1.15.2.dev0

--- cactus_test_definitions/__init__.py
+++ cactus_test_definitions/__init__.py
@@ -18,7 +18,7 @@ from cactus_test_definitions.variable_expressions import (
     try_extract_variable_expression,
 )
 
-__version__ = "1.15.1+temp"
+__version__ = "1.15.2.dev0"
 
 __all__ = [
     "TestProcedureDefinitionError",
