86ecf954540d47e4f48c03de8ccc75ecbfe91910
Lachlan <161661313+LachlanJW@users.noreply.github.com>
2026-08-11 11:03:23 +1000

    Add default der controls pt2 (#221)
    * fix to all-18 use maximport
    
    * ALL-06
    
    * ALL-08
    
    * GEN and LOA 04
    
    * ensure der program set before default
    
    * test for derp present before defaults
    
    * GEN and LOA12
    
    * update GEN and LOA 4

--- tests/unit/client/test_validate.py
+++ tests/unit/client/test_validate.py
@@ -108,6 +108,7 @@ def collect_event_param_values(tp: TestProcedure, event_type: str, param_name: s
         Path("tests/data/client/tp_invalid_bad_param.yaml"),
         Path("tests/data/client/tp_invalid_bad_step_enable.yaml"),
         Path("tests/data/client/tp_invalid_bad_step_remove.yaml"),
+        Path("tests/data/client/tp_invalid_default_control_before_program.yaml"),
     ],
 )
 def test_TestProcedure_invalid_examples(tp_file: Path):
